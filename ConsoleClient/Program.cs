edb51069d5770192f1bb6ccba71706e8cde0d2cd
danil301 <dvoryanchikovdanil@bk.ru>
2022-09-30 23:09:03 +0300

    First commit

---
+++ ConsoleClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace MyMessanger 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Worm gg m ld!");
+        }
+    }
+}
+
