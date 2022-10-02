941ed32be346273256075a1e90beab26e920f744
danil301 <dvoryanchikovdanil@bk.ru>
2022-10-02 15:25:04 +0300

    add serialization

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -1,13 +1,20 @@
 ﻿using System;
+using Newtonsoft.Json;
+
 namespace MyMessanger 
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Message msg = new Message();
-            Console.WriteLine("Hello,m gg m ld!");
-            Console.WriteLine(msg.ToString());
+            Message msg = new Message("RusAl", "Privet", DateTime.UtcNow);
+            string output = JsonConvert.SerializeObject(msg);
+            Console.WriteLine(output);
+            Message deserializedMsg = JsonConvert.DeserializeObject<Message>(output);
+            Console.WriteLine(deserializedMsg);
+            //{"UserName":"RusAl","MessageText":"Privet","TimeStamp":"2022-10-02T12:23:14.315288Z"}
+            //RusAl < 02.10.2022 12:23:14 >: Privet
+            
         }
     }
 }
