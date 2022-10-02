850e150b2674c66b24b054302e9efd5c84ea5d78
danil301 <dvoryanchikovdanil@bk.ru>
2022-10-03 00:17:13 +0300

    add post get

--- ASPCoreServer/Controllers/Messanger.cs
+++ ASPCoreServer/Controllers/Messanger.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyMessanger;
+using Newtonsoft.Json;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -8,19 +10,32 @@ namespace ASPCoreServer.Controllers
     [ApiController]
     public class Messanger : ControllerBase
     {
-        
 
+        static List<Message> ListOfMessage = new List<Message>();
         // GET api/<Messanger>/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            return "sdchbsELHC FD" + id.ToString();
+            string OutputString = "Not found";
+            if ((id < ListOfMessage.Count) && (id >= 0))
+            {
+                OutputString = JsonConvert.SerializeObject(ListOfMessage[id]);
+            }
+            Console.WriteLine(String.Format("Запрошено сообщение № {0} : {1}", id, OutputString));
+            return OutputString;
         }
 
         // POST api/<Messanger>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public IActionResult SendMessage([FromBody] Message msg)
         {
+            if (msg == null)
+            {
+                return BadRequest();
+            }
+            ListOfMessage.Add(msg);
+            Console.WriteLine(String.Format("Всего сообщений: {0} Посланное сообщение: {1}", ListOfMessage.Count, msg));
+            return new OkResult();
         }
 
        
